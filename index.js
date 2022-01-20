cce5dc3fb59d98cbf77773014ea4a06a61555e40
ntig615011 <97014149+ntig615011@users.noreply.github.com>
2022-01-20 19:22:25 +0700

    update file js

---
+++ index.js
@@ -0,0 +1,16 @@
+Đầu tiên tạo 1 repo github trống
+xong gòi
+vào source code của mình nè 
+b1: git init 
+b2: git add <tenfilemuonup hoặc dùng dấu . để up toàn bộ file trong thư mục>
+b3: git commit -m "noidungghichu"
+    git branch -M main
+b4: git remote add origin <linkrepo>
+b5: git push -u origin main
+đó là lần đầu tiên
+còn lần up tiếp theo thì chỉ cần
+git add <file>
+git commit -m "ghichu"
+git push -u origin main
+okay :< that's it ! 
+demo thuwr
